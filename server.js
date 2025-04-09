13423520ee48e80f5edb0850ddf9e569123a30ed
Shikha singh <shikhasingh@Shikhas-MacBook-Air.local>
2025-04-09 22:01:07 +0530

    Connected to MongoDB Atlas using .env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
 
 const jobRoutes = require('./routes/jobRoutes');
 
